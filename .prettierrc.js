3b15a4ba01913c08b315984edee654c94de2176b
doganozturk <doganozturk2005@gmail.com>
2019-09-02 10:48:08 +0300

    Delete redundant .prettierrc file.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,7 @@
 module.exports = {
-    bracketSpacing: false,
-    jsxBracketSameLine: true,
     singleQuote: true,
     trailingComma: 'all',
+    useTabs: false,
+    tabWidth: 4,
+    printWidth: 100,
 };
